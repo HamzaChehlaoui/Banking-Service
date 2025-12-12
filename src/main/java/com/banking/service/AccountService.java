fd5fe453d7bd2b33521a9a256247aaad0c167cab
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-12 16:26:31 +0100

    Add AccountService interface Closes #3

--- src/main/java/com/banking/service/AccountService.java
+++ src/main/java/com/banking/service/AccountService.java
@@ -4,5 +4,8 @@ import java.time.LocalDate;
 
 public interface AccountService {
 
+    void deposit(int amount ,LocalDate date );
+    void withdraw(int amount , LocalDate date);
+    void printStatement();
 
 }
