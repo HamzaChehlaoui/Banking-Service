5ef917de520843246ad31b261792c534b3931126
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-12 22:31:40 +0100

    feat: implement deposit logic in Account class Closes #5

--- src/main/java/com/banking/service/Account.java
+++ src/main/java/com/banking/service/Account.java
@@ -2,8 +2,34 @@ package com.banking.service;
 
 import com.banking.entity.Transaction;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Account implements AccountService {
 
+
+    private ArrayList<Transaction>transactions ;
+    private int  balance ;
+
+    public Account(){
+        this.transactions = new ArrayList<>();
+        this.balance = 0;
+    }
+
+    @Override
+    public void deposit(int amount , LocalDate date){
+        if(amount <=0 ){
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        if(date == null){
+            throw new IllegalArgumentException("Date cannot be null");
+        }
+
+        balance += amount ;
+
+        Transaction transaction = new Transaction(date , amount , balance);
+
+        transactions.add(transaction);
+
+    }
 }
