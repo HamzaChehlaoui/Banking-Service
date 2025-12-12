a3461ae1c7ea6f7859238ad411dd93fac7a02052
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-12 16:22:33 +0100

    Add Transaction entity Closes #2

--- src/main/java/com/banking/entity/Transaction.java
+++ src/main/java/com/banking/entity/Transaction.java
@@ -4,5 +4,25 @@ import java.time.LocalDate;
 
 public class Transaction {
 
+    private LocalDate date ;
+    private int amount ;
+    private int balanceAfterOperation ;
 
+    public Transaction(LocalDate date , int amount , int balanceAfterOperation){
+        this.date = date;
+        this.amount = amount;
+        this.balanceAfterOperation = balanceAfterOperation;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getBalanceAfterOperation() {
+        return balanceAfterOperation;
+    }
 }
