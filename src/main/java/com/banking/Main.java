4d0a106f838745ee68d7540ce211fafa98d12c11
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-13 13:16:26 +0100

    test: add main method to test Account scenario with deposits and withdrawal Closes #8

--- src/main/java/com/banking/Main.java
+++ src/main/java/com/banking/Main.java
@@ -1,13 +1,23 @@
 package com.banking;
 
-/**
- * Hello world!
- *
- */
+
+import com.banking.service.Account;
+import com.banking.service.AccountService;
+
+import java.time.LocalDate;
+
 public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        AccountService account = new Account();
+
+        account.deposit(1000, LocalDate.of(2025 , 12,13));
+        account.deposit(2000, LocalDate.of(2025,12,15));
+        account.withdraw(2000,LocalDate.of(2025,12,16));
+
+        account.printStatement();
+
     }
 }
