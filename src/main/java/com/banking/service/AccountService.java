ee854423dc1f894a2b70ddf15ec09eeec9352ea6
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-12-13 16:05:11 +0100

    refactor: fix interface to match exact requirements #9

--- src/main/java/com/banking/service/AccountService.java
+++ src/main/java/com/banking/service/AccountService.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 
 public interface AccountService {
 
-    void deposit(int amount ,LocalDate date );
-    void withdraw(int amount , LocalDate date);
+    void deposit(int amount);
+    void withdraw(int amount);
     void printStatement();
 
 }
